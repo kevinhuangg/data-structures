e9e0313c932f325a526d22d0283e4318cc59ca53
kevinhuangg <email.kevinh@gmail.com>
2017-02-08 15:04:18 -0800

    Finished stack (functional) and passed all tests

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function() {
   var someInstance = {};
-
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    if (size) {
+      var deleted = storage[size];
+      delete storage[size];      
+      size--;
+      return deleted;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
