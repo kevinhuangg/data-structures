f099b972ba3c11693bfc7c40bdbe927dab5df4a8
kevinhuangg <email.kevinh@gmail.com>
2017-02-08 20:15:07 -0800

    wrote functions queue and stack (prototypal) and passed all tests

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queue = Object.create(queueMethods);
+  queue.length = 0;
+  queue.storage = {};
+
+  return queue;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function() {
+  return this.length;
+};
+queueMethods.enqueue = function(val) {
+  this.length++;
+  this.storage[this.length] = val; 
+};
+queueMethods.dequeue = function() {
+  if (this.length) { //breaks if we add the length second
+    var deleted = this.storage[1];
+    delete this.storage[1];
+    for (var i = this.length; i > 1; i--) {
+      this.storage[this.length - 1] = this.storage[this.length];
+    }
+    this.length--;
+    return deleted;
+  }
+};
 
 
