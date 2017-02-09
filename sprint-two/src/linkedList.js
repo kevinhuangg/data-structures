d80c7847a065e78ebf5fb57d6de07868dfda4e90
kevinhuangg <email.kevinh@gmail.com>
2017-02-09 15:39:50 -0800

    finished linkedList class function and passed all tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,20 +4,51 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    if (this.head === null) { //case for 1 node
+      this.head = node;
+    } else { //case for 2 nodes
+      this.head.next = node;
+    }
+    this.tail = node;
   };
 
   list.removeHead = function() {
+    var oldHead = this.head;
+    if (this.head !== null) {
+      this.head = oldHead.next;
+    }
+    return oldHead.value;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    var node = node || this.head;
+    if (node.value === target) { //if current node contains target value return true
+      return true;
+    }
+    if (node.next === null) { //if we reach end of the list and nothing
+      return false;           //has been found return false
+    }
+    return this.contains(target, node.next);
+
+    // var pointer = this.head;
+
+    // while (pointer) {
+    //   if (pointer.value === target) {
+    //     return true;
+    //   }
+    //   pointer = pointer.next;
+    // }
+
+    // return false;
   };
+
 
   return list;
 };
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
 
