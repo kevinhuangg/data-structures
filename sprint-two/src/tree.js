c6c996011c049faf920a709a354c6ef86805e9b2
kevinhuangg <email.kevinh@gmail.com>
2017-02-09 17:59:01 -0800

    finished tree function class and passed all tests

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.children = [];
 
-  // your code here
-  newTree.children = null;  // fix me
+  // extend treeMethods into newTree
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +12,26 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var treeNode = new Tree(value);
+  this.children.push(treeNode);
 };
 
 treeMethods.contains = function(target) {
+
+  var found = false;
+  var checkTree = function(node) {
+    if (node.value === target) {
+      found = true;
+    }
+    if (node.children && !found) {
+        //loop over children array
+      for (var i = 0; i < node.children.length; i++) {
+        checkTree(node.children[i]);
+      }
+    }  
+  };
+  checkTree(this);
+  return found;
 };
 
 
