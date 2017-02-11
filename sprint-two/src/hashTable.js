4d5810a8a68b19bd5e90c855c47c43b54566f1ad
kevinhuangg <email.kevinh@gmail.com>
2017-02-10 21:27:21 -0800

    Finished hash table function class and tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,36 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index] === undefined) { //if theres nothing at that index
+    this._storage[index] = [];//add a bucket
+  }
+  this._storage[index].push([k, v]);
+
+  if (this._storage[index][0][0] === k) { //checks for repeate keys and replaces v
+    this._storage[index][0][1] = v;
+  } else if (this._storage[index][1][0] === k) {
+    this._storage[index][1][1] = v;
+  }
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[index][0] && this._storage[index][0][0] === k) {
+    return this._storage[index][0][1];
+  }
+  if (this._storage[index][0] && this._storage[index][1][0] === k) {
+    return this._storage[index][1][1];
+  }
+
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[index] = this._storage[index].filter(function(val) {
+    return val[0] !== k;
+  });
+
 };
 
 
